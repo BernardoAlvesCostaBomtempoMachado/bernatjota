1df9a32e62e72986f8ac5ddc0253a3c9be58ff65
BernardoAlvesCostaBomtempoMachado <105887787+BernardoAlvesCostaBomtempoMachado@users.noreply.github.com>
2024-08-21 08:49:04 -0300

    l

---
+++ script.js
@@ -0,0 +1,71 @@
+const caixaPrincipal = document.querySelector ('.caixa-principal');
+const caixaPerguntas = document.querySelector ('.caixa-perguntas');
+const caixaAlternativas = document.querySelector ('.caixa-alternativas');
+const caixaResultado = document.querySelector ('.caixa-resultado');
+const textoResultado = document.querySelector ('.texto-resultado');
+
+const alternativas = [
+    {
+        enunciado: "No âmbito social, você prefere:",
+        alternativa: [
+            {
+                textp: "Alternativa 1",
+                afirmaçao: "Afirmação 1",
+            },
+            {
+                textp: "Alternativa 2",
+                afirmaçao: "Afirmação 2",
+            },
+        ]
+        
+    },
+
+    {
+        enunciado: "No âmbito ambiental, você prefere:",
+        alternativa: [
+            {
+                textp: "Alternativa 3",
+                afirmaçao: "Afirmação 3",
+            },
+            {
+                textp: "Alternativa 4",
+                afirmaçao: "Afirmação 4",
+            },
+        ]
+        
+    },
+
+    {
+        enunciado: "No âmbito tecnológico, você prefere:",
+        alternativa: [
+            {
+                textp: "Alternativa 5",
+                afirmaçao: "Afirmação 5",
+            },
+            {
+                textp: "Alternativa 6",
+                afirmaçao: "Afirmação 6",
+            },
+        ]
+        
+    },
+]
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPerguntas () {
+    perguntaAtual = alternatias [atual];
+    caixaAlternativas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent= "";
+}
+
+function mostraAlternativas(){
+   for (const opcao of perguntaAtual.alternativas) {
+        const botaoAlternativa = document.createElement('button');
+        botaoAlternativa.textContent = opcao.texto;
+        caixaAlternativas.appendChild(botaoAlternativa);
+   }
+}
+
+mostraPerguntas();
